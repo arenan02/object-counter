87c64c4a30c803b9d90664354cead8864425620a
Nishanth Rajamani <nshnp@ramboll.com>
2024-12-06 12:42:03 +0000

    Add integration test for prediction endpoint;

--- tests/entrypoints/test_webapp.py
+++ tests/entrypoints/test_webapp.py
@@ -42,3 +42,28 @@ def test_object_detection(client, image_path):
     # and status code is correct(Integration test)
     assert response.status_code == 200
     assert json.loads(response.data) is not None
+
+def test_predict(client, image_path):
+    # Load the image from the path resource/boy.jpg
+    with open(image_path, "rb") as f:
+        image_data = f.read()
+    image = io.BytesIO(image_data)
+
+    data = {
+        "threshold": "0.9",
+        "model_name": "rfcn",
+    }
+    data["file"] = (image, "test.jpg")
+
+    # Make a test request to the object_detection endpoint
+    response = client.post(
+        "/predict", 
+        data=data, 
+        content_type="multipart/form-data", 
+        buffered=True
+    )
+
+    # Check that the predict_action was called with the correct arguments and
+    # and status code is correct(Integration test)
+    assert response.status_code == 200
+    assert json.loads(response.data) is not None
